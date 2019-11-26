c7751119bc6046b2bc432b85a9574c0a3765eab3
Thiago <thibousbr@gmail.com>
2019-11-26 18:14:37 -0300

    Concertado nomes não condizentes

--- src/server/app/ServerController.ts
+++ src/server/app/ServerController.ts
@@ -1,15 +1,16 @@
 import express from 'express'
+import http from 'http'
 var path = require('path');
 
 export default class ServerController{
 
   app: any;
   server_dir: string;
-  server: any;
+  server:any;
 
   constructor(){
     this.app = express();
-    this.server = require('http').Server(this.app);
+    this.server = http.createServer(this.app);
     this.server_dir = __dirname + '/../../../src';
   }
 
@@ -22,7 +23,23 @@ export default class ServerController{
     
     this.app.use('/client', express.static(path.resolve(this.server_dir + '/client')))
     
-    console.log("Server alive");
-    this.server.listen(2000);
+    let port = 2000
+    this.server.listen(port, ()=>{
+      console.log("Server alive");
+    });
+  }
+
+  printIp(){
+    var os = require('os');
+    var interfaces = os.networkInterfaces();
+    var addresses = [];
+    for(var k in interfaces) {
+      var inter = interfaces[k]
+      for(var j in inter)
+        if(inter[j].family === 'IPv4' && !inter[j].internal)
+        addresses.push( inter[j].address)
+    }
+    console.log(addresses);
   }
 }
+
--- src/server/app/Simulation/Neural/Genetic/FlappyGenetic.ts
+++ src/server/app/Simulation/Neural/Genetic/FlappyGenetic.ts
@@ -6,11 +6,12 @@ export default class FlappyGenetic extends InterfaceGeneticAlgorithm<NeuralBird>
 
 
   public makeObjsPerform(){
-    this.objects.forEach((val:NeuralBird)=>{
-      if(val.alive){
-        let wall = this.objects_controller.findObjectLessDistance(val, 'wall-down')
-        let dist = (wall.position.x + wall.width) - val.position.x
-        val.perform([dist, wall.position.y, val.position.y, val.velocity.y])
+    this.objects.forEach((bird:NeuralBird)=>{
+      if(bird.alive){
+        let wall = this.objects_controller.findObjectLessDistance(bird, 'wall-down')
+        let dist = wall.position.x + wall.width
+
+        bird.perform([dist, wall.position.y, bird.position.y, bird.velocity.y])
       }
     })
   }
